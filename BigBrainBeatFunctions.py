546df6b470b5795c2a18252a791ddbc05ba53f5f
Vanin Ferrall <49698503+Ferrallv@users.noreply.github.com>
2019-09-25 17:59:20 -0400

    Updated graphs
    Changed the graph output in the presentation notebook to use bokeh instead of pyplot.

--- BigBrainBeatFunctions.py
+++ BigBrainBeatFunctions.py
@@ -3,6 +3,8 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
+from bokeh.layouts import column
+from bokeh.plotting import figure, output_notebook, show
 
 # Base packages used
 import keras
@@ -34,7 +36,7 @@ import random
 # To resize the spectrogram!
 import cv2
 
-# This is the model that we are currently using.
+# This is the model that we are currently using during the testing of these functions.
 BigBrainBeatv3_phase2 = load_model('BigBrainBeatv3_phase2.h5')
 
 # A function to read in a wav, take the first four seconds, then arrange it to do a prediction
@@ -54,12 +56,13 @@ def new_wav_slice(trackpath = "Teleportoise - Epilogue.wav", start = 0):
     # To normalize the wav
     wav = wav / np.max(wav)
 
-    start = sr*start
+    # To determine the beginning of the wave slice.
+    start = int(sr*start)
 
     # To take our desired slice
     x_slice = wav[start:(start+(sr*4))]
 
-    # To downsample our x_slice
+    # To downsample our x_slice to 11025Hz
     x_slice = x_slice[0::4]
 
     # To reshape into what we want for the model
@@ -72,11 +75,10 @@ def graph_pred(three_d_mat, model):
     This function takes in a matrix shape(1, 441, 100), and our model.
     The matrix should represent a 4 second 44100hz wav, downsampled to 11025hz and reshaped.
     This function then predicts a bpm based on the range our model has been provided.
-    It returns a beat prediction array as well a flattening of the wave matrix to compare in a graph.
+    It returns a beat prediction array as well a flattening of the wave matrix to compare to the beats in a graph.
     """
     # To get our flattened file for later.
     downsampled_wav = three_d_mat.flatten()
-
 
     # A list of possible bpm values
     category_list = list(np.arange(80,161))
@@ -103,18 +105,23 @@ def graph_pred(three_d_mat, model):
 
     # To write in where we are inferring the beats should be based on predicition
     for i in range(index_start,44101,beat_step):
-        beat_pred[i-1] = 1
+        beat_pred[i-1] = downsampled_wav.max()
 
 
     return beat_pred, downsampled_wav
+
+
+# Here we are creating the "pipeline" function. This is essentially an envelope to run the other two functions
+# appropriately, while also giving us visualizations of the data.
 
 def show_me(trackpath = "WaveBank/two_110.wav", model = BigBrainBeatv3_phase2, seconds = 0):
     """
     This function takes a path to a .wav, runs it through a chosen model, and presents
     the predicted bpm, the downsampled waveform, and the visualization of the matrix used for analysis.
 
-    We will also be able to listen to a stereo 4 second track. The left speaker will play the downsampled audio,
-    and the right speaker will be the inferred beats.
+    With the returned variables we will be able to listen to a stereo 4 second track. The left speaker will play the downsampled audio,
+    and the right speaker will be the inferred beats. To do this, we will need to run the IPython.Display.Audio method, using our
+    returned variables for the input data, as well as the appropriate sample rate (11025). In future, this feature will likely be embedded in this function.
 
     ---
     Argument description:
@@ -136,6 +143,9 @@ def show_me(trackpath = "WaveBank/two_110.wav", model = BigBrainBeatv3_phase2, s
     # To reshape our matrix so that we might visualize it.
     matrix = matrix.reshape(441,100)
 
+    
+    
+    """ # This is the previous code with graphs. Below will be our new graphs using bokeh!
     # Plotting our results
     plt.subplots(2,1,figsize=(15,10))
 
@@ -146,6 +156,21 @@ def show_me(trackpath = "WaveBank/two_110.wav", model = BigBrainBeatv3_phase2, s
     plt.subplot(2,1,2)
     plt.imshow(matrix.T)
 
-    plt.show();
+    plt.show();"""
+    
+    # To make sure it comes out in the notebook
+    output_notebook()
+    
+    # Our first viz, this will show the audio slice with the inferred beats superimposed
+    p1 = figure(plot_width=1000, plot_height=400, title="Downsampled Wave with Inferred Beats")
+    p1.line(np.arange(0,len(downsampled_wav)), downsampled_wav)
+    p1.line(np.arange(0,len(downsampled_wav)), beat_pred, line_color="red", line_width=3)
 
+    # The second viz, which will be the "image" we show the CNN
+    p2 = figure(plot_width=1000, plot_height=400, title="Visualization of Audio Slice")
+    p2.image(image=[matrix.T], x=0, y=0, dw=441, dh=100, palette="Viridis256")
+
+
+    show(column(p1,p2));
+    
     return downsampled_wav, beat_pred
